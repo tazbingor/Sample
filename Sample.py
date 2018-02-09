f858691a8ce4668eb4c8b79c487076deef9d4ed1
tazbingor <852353298@qq.com>
2018-02-09 23:19:11 +0800

    flask hello world

--- Sample.py
+++ Sample.py
@@ -1,12 +1,26 @@
-from flask import Flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # return 'Hello World!'
+    return render_template('index.html', title='hello world')
+
+
+@app.route('/services')
+def services():
+    return 'Service'
+
+
+@app.route('/about')
+def about():
+    return 'About'
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.debug = True
+    app.run(debug=True)
