1a068b7acda7ec53fc90551444179683f5df902f
tazbingor <852353298@qq.com>
2018-02-10 23:11:18 +0800

    flask hello world

--- Sample.py
+++ Sample.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # return 'Hello World!'
-    return render_template('index.html', title='hello world')
+    return render_template('index.html', title='Sample Blog')
 
 
 @app.route('/services')
