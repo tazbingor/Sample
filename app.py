ce4e433ff5bdee5c6484fd810c7d2fa815897713
tazbingor <852353298@qq.com>
2018-02-18 22:24:00 +0800

    flask script runserver

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+from livereload import Server
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           title="<h1>你好,吃了么</h1>",
+                           body="## Header2")
+
+
+@app.template_filter('md')
+def markdown_to_html(txt):
+    from markdown import markdown
+    return markdown(txt)
+
+
+if __name__ == '__main__':
+    live_server = Server(app.wsgi_app)
+    live_server.watch('**/*.*')
+    live_server.serve(open_url=True)
