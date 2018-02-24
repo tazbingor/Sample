382a5a951119704b95e3e64e295f76c0af20d6a7
tazbingor <852353298@qq.com>
2018-02-25 00:12:08 +0800

    web blog

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+from os import path
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = path.abspath(path.dirname(__file__))
+db = SQLAlchemy
+bootstrap = Bootstrap()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config')
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + path.join(basedir, 'data.sqlite')
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+    db.init_app(app)
+    bootstrap.init_app(app)
+
+    from auth import auth as  auth_blueprint
+    from main import main as  main_blueprint
+
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    app.register_blueprint(main_blueprint, static_folder='static')
+
+    return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, request
+from . import main
+
+@main.route('/')
+def index():
+    return render_template('index.html', title='Welcome')
 
 
-def init_views(app):
-    @app.route('/')
-    def index():
-        return render_template('index.html', title='Welcome')
+@main.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('404.html'), 404
 
-    @app.template_test('current_link')
-    def is_current_link(link):
-        return link == request.path
+@main.template_test('current_link')
+def is_current_link(link):
+    return link == request.path
 
-    @app.route('/user/<regex("[a-zA-Z]{3}"):user_id>/')
-    def user(user_id):
-        return 'User %s' % user_id
 
-    @app.route('/services/')
-    def services():
-        return 'Service'
+@main.route('/user/<regex("[a-zA-Z]{3}"):user_id>/')
+def user(user_id):
+    return 'User %s' % user_id
 
-    @app.route('/about/')
-    def about():
-        return 'About'
 
-    @app.route('/projects/')
-    @app.route('/works/')
-    @app.route('/our-works/')
-    def projects():
-        '''
-        多个路由响应一个方法
-        :return:
-        '''
-        return 'The project page'
+@main.route('/services/')
+def services():
+    return 'Service'
+
+
+@main.route('/about/')
+def about():
+    return 'About'
+
+
+@main.route('/projects/')
+@main.route('/works/')
+@main.route('/our-works/')
+def projects():
+    '''
+    多个路由响应一个方法
+    :return:
+    '''
+    return 'The project page'
 
     # @app.route('/login/', methods=['GET', 'POST'])
     # def login():
@@ -45,13 +50,13 @@ def init_views(app):
     #     flash(u'登录成功!')
     #     return render_template('login.html', title=u'登录', form=form)
 
-        # @app.route('/upload/', methods=['GET', 'POST'])
-        # def upload():
-        #     if request.method == 'POST':
-        #         f = request.files['file']
-        #         basepath = path.abspath(path.dirname(__file__))
-        #         upload_path = path.join(basepath, 'static/uploads')
-        #         f.save(upload_path, secure_filename(f.filename))
-        #         return redirect(url_for('upload'))
-        #
-        #     return render_template('upload.html')
+    # @app.route('/upload/', methods=['GET', 'POST'])
+    # def upload():
+    #     if request.method == 'POST':
+    #         f = request.files['file']
+    #         basepath = path.abspath(path.dirname(__file__))
+    #         upload_path = path.join(basepath, 'static/uploads')
+    #         f.save(upload_path, secure_filename(f.filename))
+    #         return redirect(url_for('upload'))
+    #
+    #     return render_template('upload.html')
